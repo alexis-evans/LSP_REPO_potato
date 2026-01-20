5b00fdabbe052c1b859c83cb66a9688e6c8072b2
alexis-evans <alevans989@gmail.com>
2026-01-20 14:24:44 -0500

    Adding HelloWorld.java to repository

---
+++ src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Name: Alexis Evans
+ */
+
+package org.howard.edu.lsp.assignment1;
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
